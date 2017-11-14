d9ae45b6fb7a3842e7c3149ddbdec7fc54168ff0
kexun <1193747537@qq.com>
2017-11-14 14:58:19 +0800

    test

--- config/app.php
+++ config/app.php
@@ -176,6 +176,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /*
+         * aetherupload-laravel
+         */
+        AetherUpload\AetherUploadServiceProvider::class,
 
     ],
 
