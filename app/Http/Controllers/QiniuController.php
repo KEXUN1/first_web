41fb5a28538fc7597dec6917cc58342447c09430
kexun <1193747537@qq.com>
2017-11-14 12:50:55 +0800

    test

--- app/Http/Controllers/QiniuController.php
+++ app/Http/Controllers/QiniuController.php
@@ -9,8 +9,8 @@ use Qiniu\Auth;
 class QiniuController extends Controller
 {
     public function radioUpload(Request $request){
-        $accessKey ="6uRB0cKNqK8Y-da8AlV7L5EvKt_bpeGXWcP9on3x";
-        $secretKey = "hj0GN95V_b-wt5t06To7MEx2aE6UHHRC_QhuDvL6";
+        $accessKey ="cPkNshaC3txFX84Q_R9OoanMWd24QfHa8tvcS9G";
+        $secretKey = "osdchuL7-bx7PNbwM0RQ1d26H_L1t9GWgX6IYzof";
         $bucket = "shifangyunshui";
         // 构建鉴权对象
         $auth = new Auth($accessKey, $secretKey);
